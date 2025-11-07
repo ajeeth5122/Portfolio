75fc109ebbd1b682a82e655e761869a7fd9d472f
Ajeeth <ajihkettaven@gmail.com>
2025-11-07 14:22:40 +0530

    ad3

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home/>} />
+        {/* <Route path='/' element={<Home/>} /> */}
          <Route path='/Home' element={ <Home/> } />
           <Route path='/About' element={ <About/> } />
            <Route path='/Project' element={<Project/>} />

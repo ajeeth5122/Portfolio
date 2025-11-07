573914b2b20226e43e05532efc5473a3735e41f5
Ajeeth <ajihkettaven@gmail.com>
2025-11-07 14:41:18 +0530

    1321

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from './Component/Header'
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'
 import About from './pages/About'
 import Project from './pages/Project'
@@ -12,8 +12,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path='/' element={<Home/>} /> */}
-         <Route path='/Home' element={ <Home/> } />
+        <Route path='/' element={<Home/>} />
+         {/* <Route path='/Home' element={ <Home/> } /> */}
           <Route path='/About' element={ <About/> } />
            <Route path='/Project' element={<Project/>} />
             <Route path='/Contact' element={<Contact/>} />

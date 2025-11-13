7d8294c9a183cf958a99e59bccf5947f4ef74a80
Ajeeth <ajihkettaven@gmail.com>
2025-11-13 19:38:30 +0530

    css mod

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,8 +4,17 @@ import Header from '../Component/Header'
 function Contact() {
   return (
      <>
-   
       <Header/>
+      <div className='contact'>
+        
+      <form type='submit'>
+        <h1 className='C-title'>For Contact</h1>
+        <input type="text" placeholder='Name'/>
+        <input type="mail" placeholder='E-Mail'/>
+        <textarea name="text" placeholder='Enter Your Message' id=""></textarea>
+        <button>Submit</button>
+      </form>
+      </div>
     
     </>
   )

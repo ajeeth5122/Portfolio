5a8b115f2a30f06b9482883c24b647a663a03088
Ajeeth <ajihkettaven@gmail.com>
2025-11-17 17:13:14 +0530

    css chg

--- src/Projects/TodoList.jsx
+++ src/Projects/TodoList.jsx
@@ -4,71 +4,76 @@ import { Navigate, useNavigate } from 'react-router-dom';
 
 function TodoList() {
   const navigate = useNavigate();
-    const [todo,setTodo]=useState([]);
-    const inputRef=useRef();
-    const date= new Date()
-    const time=date.getHours();
-    const mins= date.getMinutes();
+  const [todo, setTodo] = useState([]);
+  const inputRef = useRef();
+  const date = new Date()
+  const time = date.getHours();
+  const mins = date.getMinutes();
 
 
-    const HandleAdd=(e)=>{
-        const text=inputRef.current.value.trim();
+  const HandleAdd = (e) => {
+    const text = inputRef.current.value.trim();
 
-        if (text === '')return alert ("add Something")
-       const newtodo={
-        id: Date.now(),
-        text: text,
-        complete: false,
-        min: mins,
-        hour: time
-       }
-       setTodo((prev)=>[...prev,newtodo]);
-       inputRef.current.value="";
+    if (text === '') return alert("add Something")
+    const newtodo = {
+      id: Date.now(),
+      text: text,
+      complete: false,
+      min: mins,
+      hour: time
     }
-    const HandleDelete=(id)=>{
-        setTodo(todo.filter((task)=>task.id !== id))
-    }
-    const HandleComplete=(id)=>{
-       setTodo(todo.map((task)=>
-        task.id === id ? {...task, complete : !task.complete} : task
-      ))
-     }
+    setTodo((prev) => [...prev, newtodo]);
+    inputRef.current.value = "";
+  }
+  const HandleDelete = (id) => {
+    setTodo(todo.filter((task) => task.id !== id))
 
-    console.log(todo)
+  }
+  const HandleComplete = (id) => {
+    setTodo(todo.map((task) =>
+      task.id === id ? { ...task, complete: !task.complete } : task,
+
+    ))
+
+  }
+
+  console.log(todo)
   return (
     <>
-   <i onClick={(e)=>navigate("/")  } className=" home bi bi-house-fill">Home</i>
-    <div className='Todo' >
+      <i onClick={(e) => navigate("/")} className=" home bi bi-house-fill">Home</i>
+
+      <div className='Todo' >
+        <div className='listTitle'>
+          <i className="bi bi-list-task"></i>
+          <h1>Todo List</h1>
+        </div>
         <div className='lists' >
-            <div className='listTitle'>
-                <i className="bi bi-list-task"></i>
-                <h1>Todo List</h1>
-            </div>
-           
-           <div className='inputbutton'>
+
+
+          <div className='inputbutton'>
             <input type="text" ref={inputRef} placeholder='Enter your Task' />
             <i onClick={HandleAdd} className="bi bi-patch-plus"></i>
-           </div>
-        </div>
-        
-    
-    <div className='todos'>
-        { todo.map((todos)=>
-        <div className={ todos.complete ? 'complete' : 'task'}key={todos.id}>
-          <h1>{todos.text}</h1>
-          {todos.mins <= 9 ? <p>Time:{todos.hour}:0{todos.min}</p> : <p>Time:{todos.hour}:{todos.min}</p> }
-          <div className='todo-btn'>
-            {todos.complete ? <button onClick={() => HandleDelete(todos.id)} className='comp'>Clear</button> : 
-            <div className='todo-btn'>
-          <button onClick={() => HandleComplete(todos.id)} className='comp'>complete</button>
-          <button onClick={() => HandleDelete(todos.id)} className='del'>delete</button>
           </div>
-            }
-          </div>
-        </div> )
-       }
         </div>
+
+
+        <div className='todos'>
+          {todo.map((todos) =>
+            <div className={todos.complete ? 'complete' : 'task'} key={todos.id}>
+              <h1>{todos.text}</h1>
+              {todos.mins <= 9 ? <p>Time:{todos.hour}:0{todos.min}</p> : <p>Time:{todos.hour}:{todos.min}</p>}
+              <div className='todo-btn'>
+                {todos.complete ? <button onClick={() => HandleDelete(todos.id)} className='comp'>Clear</button> :
+                  <div className='todo-btn'>
+                    <button onClick={() => HandleComplete(todos.id)} className='comp'>Complete</button>
+                    <button onClick={() => HandleDelete(todos.id)} className='del'>Delete</button>
+                  </div>
+                }
+              </div>
+            </div>)
+          }
         </div>
+      </div>
     </>
   )
 }
